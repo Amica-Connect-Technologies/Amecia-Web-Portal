c8d175d99540acc84fe0a86c914ebcfce0466666
Ali Raza <155522142+AliRazaDeveloper75@users.noreply.github.com>
2026-01-10 10:17:48 +0500

    Delete backend directory

--- backend/profiles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CreateProfileAPIView, GetUpdateProfileAPIView
-
-urlpatterns = [
-    path('create/', CreateProfileAPIView.as_view(), name='create_profile'),
-    path('me/', GetUpdateProfileAPIView.as_view(), name='get_update_profile'),
-]
